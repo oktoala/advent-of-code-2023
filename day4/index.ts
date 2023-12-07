8124817d68d0f3b7f0c94367ae5c1590bc40e8db
oktoala <okta.yoga.syah@gmail.com>
2023-12-07 14:53:04 +0800

    Done day4 part1

---
+++ day4/index.ts
@@ -0,0 +1,34 @@
+const path = "./input.txt";
+const file = Bun.file(path);
+
+const lineText = await file.text();
+
+const cards = lineText.split("\n").filter((v) => !!v);
+
+let answer = 0;
+cards.forEach((card) => {
+  const cardSplit = card.split("|");
+
+  const leftCards = cardSplit[0]
+    .split(":")[1]
+    .split(" ")
+    .filter((v) => !!v);
+
+  const rightCards = cardSplit[1]
+    .normalize()
+    .split(" ")
+    .filter((v) => !!v);
+
+  let score = 0;
+  rightCards.forEach((card) => {
+    if (leftCards.includes(card) && score === 0) {
+      score += 1;
+    } else if (leftCards.includes(card) && score > 0) {
+      score *= 2;
+    }
+  });
+
+  answer += score;
+});
+
+console.log(answer);

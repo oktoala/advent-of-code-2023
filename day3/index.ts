c7fa30b8114ac145bec9fb0ef598497447c2c25d
oktoala <okta.yoga.syah@gmail.com>
2023-12-07 14:22:58 +0800

    Done day3 part2

--- day3/index.ts
+++ day3/index.ts
@@ -10,11 +10,14 @@ function isNumber(num: string) {
 }
 
 function isSymbol(str: string) {
-  const v = !isNumber(str) && str !== ".";
+  const v = str === "*";
   return v;
 }
 
+type Star = Record<string, number[]>;
+
 const schemas: number[] = [];
+let currStar: Star = {};
 lines.forEach((line, i) => {
   const prevLine = i - 1 < 0 ? undefined : lines[i - 1].split("");
   const nextLine =
@@ -26,6 +29,8 @@ lines.forEach((line, i) => {
   let startIndex: number | null = null;
   const bandIndex: number[] = [];
   words.forEach((letter, ii) => {
+    let location: string | null = null;
+    let tempCurrStar: Star = {};
     const isLefter = ii === 0;
     const isRighter = ii === words.length - 1;
 
@@ -38,34 +43,42 @@ lines.forEach((line, i) => {
       }
 
       if (prev && !findSymbol) {
+        location = `${i}-${ii - 1}`;
         findSymbol = isSymbol(prev);
 
         if (prevLine && !findSymbol) {
           findSymbol = isSymbol(prevLine[ii - 1]);
+          location = `${i - 1}-${ii - 1}`;
         }
 
         if (nextLine && !findSymbol) {
           findSymbol = isSymbol(nextLine[ii - 1]);
+          location = `${i + 1}-${ii - 1}`;
         }
       }
 
       if (prevLine && !findSymbol) {
         findSymbol = isSymbol(prevLine[ii]);
+        location = `${i - 1}-${ii}`;
       }
 
       if (nextLine && !findSymbol) {
         findSymbol = isSymbol(nextLine[ii]);
+        location = `${i + 1}-${ii}`;
       }
 
       if (next && !findSymbol) {
         findSymbol = isSymbol(next);
+        location = `${i}-${ii + 1}`;
 
         if (prevLine && !findSymbol) {
           findSymbol = isSymbol(prevLine[ii + 1]);
+          location = `${i - 1}-${ii + 1}`;
         }
 
         if (nextLine && !findSymbol) {
           findSymbol = isSymbol(nextLine[ii + 1]);
+          location = `${i + 1}-${ii + 1}`;
         }
       }
     } else {
@@ -73,24 +86,42 @@ lines.forEach((line, i) => {
       startIndex = null;
     }
 
-    if (startIndex !== null && findSymbol) {
+    if (startIndex !== null && findSymbol && !!location) {
       let temp = "";
       const vv = line.substring(startIndex).split("");
-      // console.log(line.substring(startIndex));
+      if (location === "1136") {
+        console.log(line);
+      }
 
       vv.some((v, j) => {
         if (!isNumber(v)) return true;
         temp += v;
-        bandIndex.push(startIndex + j);
+        if (startIndex) {
+          bandIndex.push(startIndex + j);
+        }
       });
-      console.log(bandIndex);
       schemas.push(Number(temp));
-      // console.log(line.substring(startIndex));
+      if (!currStar[location]) {
+        currStar[location] = [];
+      }
+      currStar[location] = [...currStar[location], Number(temp)];
+      if (location === "1136") {
+        // console.log(line);
+        console.log(i, ii);
+        console.log(location);
+      }
     }
   });
 });
 
+const ger = Object.keys(currStar)
+  .filter((k) => currStar[k].length > 1)
+  .reduce((obj, key) => {
+    return obj + currStar[key].reduce((a, b) => a * b);
+  }, 0);
+
+console.log(ger);
 Bun.write("./output.txt", schemas.join("\n"));
 // console.log(schemas);
 const answer = schemas.reduce((a, b) => a + b, 0);
-console.log(answer);
+// console.log(answer);

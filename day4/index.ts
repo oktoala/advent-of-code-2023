14528587c9d856bf836e7870b0c14be3f1f3c8b2
oktoala <okta.yoga.syah@gmail.com>
2023-12-07 15:58:59 +0800

    Done day4 part2 (not optimal) ಥ‿ಥ

--- day4/index.ts
+++ day4/index.ts
@@ -1,15 +1,22 @@
-const path = "./test.txt";
+const path = "./input.txt";
 const file = Bun.file(path);
 
 const lineText = await file.text();
 
 const cards = lineText.split("\n").filter((v) => !!v);
 
-let answer = 0;
+type Cards = Record<string, number>;
+const c: Cards = {};
+
 cards.forEach((card, i) => {
   const cardNumber = i + 1;
-  console.log(cardNumber);
   const cardSplit = card.split("|");
+
+  console.log(`Kartu-${cardNumber}`);
+
+  if (!c[cardNumber] === undefined) {
+    c[cardNumber] = 0;
+  }
 
   const leftCards = cardSplit[0]
     .split(":")[1]
@@ -22,15 +29,47 @@ cards.forEach((card, i) => {
     .filter((v) => !!v);
 
   let score = 0;
+
+  // Looping Kartu Extra
+  for (let indexx = 0; indexx < c[cardNumber]; indexx++) {
+    let scoreExtra = 0;
+    rightCards.forEach((card) => {
+      if (leftCards.includes(card)) {
+        scoreExtra += 1;
+      }
+    });
+
+    for (let index = 0; index < scoreExtra; index++) {
+      if (!c[cardNumber + index + 1]) {
+        c[cardNumber + index + 1] = 0;
+      }
+      c[cardNumber + index + 1] += 1;
+    }
+  }
+
+  // Looping Kartu Original
   rightCards.forEach((card) => {
-    if (leftCards.includes(card) && score === 0) {
+    if (leftCards.includes(card)) {
       score += 1;
-    } else if (leftCards.includes(card) && score > 0) {
-      score *= 2;
     }
   });
 
-  answer += score;
+  // Ngitung udah berapa yang menang
+  for (let index = 0; index < score; index++) {
+    if (!c[cardNumber + index + 1]) {
+      c[cardNumber + index + 1] = 0;
+    }
+    c[cardNumber + index + 1] += 1;
+  }
+
+  if (!c[cardNumber]) {
+    c[cardNumber] = 0;
+  }
+  c[cardNumber] += 1;
+
+  console.log(`End of Kartu-${cardNumber}`);
 });
+
+const answer = Object.keys(c).reduce((a, b) => a + c[b], 0);
 
 console.log(answer);

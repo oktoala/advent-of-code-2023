951104ac92ed937cd5fba939df83641fb6620f79
oktoala <okta.yoga.syah@gmail.com>
2023-12-09 14:32:38 +0800

    Done day6 part1 (easy) (◍•ᴗ•◍)

---
+++ day6/index.ts
@@ -0,0 +1,39 @@
+const path = "./test.txt";
+const file = Bun.file(path);
+
+const lineText = await file.text();
+
+const arr = lineText
+  .split("\n")
+  .filter((v) => !!v)
+  .map((v) => v.match(/\d+/g)?.map((vv) => Number(vv)));
+
+let range: number[][] = [];
+arr.forEach((a, i) => {
+  a?.forEach((aa, ii) => {
+    range.push([]);
+    range[ii].push(aa);
+  });
+});
+
+range = range.filter((v) => v.length !== 0);
+
+const arrayAmount: number[] = [];
+range.some(([time, beatDistance]) => {
+  let amount = 0;
+  for (let hold = 0; hold <= Math.floor(time / 2); hold++) {
+    const move = time - hold;
+    const distance = hold * move;
+
+    if (distance > beatDistance) {
+      amount += 2;
+    }
+  }
+  if (time % 2 === 0) {
+    amount -= 1;
+  }
+  arrayAmount.push(amount);
+});
+console.log(arrayAmount);
+const answer = arrayAmount.reduce((a, b) => a * b, 1);
+console.log(answer);

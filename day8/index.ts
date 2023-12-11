ae91de9cf166ad664c93f4d7dd9b144c3355e77b
oktoala <okta.yoga.syah@gmail.com>
2023-12-11 15:48:19 +0800

    Done day8 part2 (LCM made a problem) ಠ︵ಠ

--- day8/index.ts
+++ day8/index.ts
@@ -2,7 +2,7 @@ const path = "./input.txt";
 const file = Bun.file(path);
 
 const lcm = (...arr: number[]) => {
-  const gcd = (x: number, y: number) => (!y ? x : gcd(y, x % y));
+  const gcd = (x: number, y: number): number => (!y ? x : gcd(y, x % y));
   const _lcm = (x: number, y: number) => (x * y) / gcd(x, y);
   return [...arr].reduce((a, b) => _lcm(a, b));
 };
